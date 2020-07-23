0eddc6dd2c6de62c4c4975ccd2747b98e9fbeab0
Elisa BOURG <zaza3355@hotmail.fr>
2020-07-23 11:52:02 +0200

    start parti portfolio

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Education;
 use App\Entity\Skill;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,6 +19,18 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'skills'         =>    $skills
+        ]);
+    }
+    /**
+     * @Route("/my-projects", name="my_projects")
+     */
+    public function showProjects()
+    {
+        $educationRepository = $this->getDoctrine()->getRepository(Education::class);
+        $educations = $educationRepository->findAll();
+        return $this->render('home/my_projects.html.twig', [
+            'controller_name' => 'HomeController',
+            'educations'      =>  $educations,
         ]);
     }
 }
