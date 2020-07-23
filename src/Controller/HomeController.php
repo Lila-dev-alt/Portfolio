655f76a340b60abfcb3568c6d997dc785fbc5f70
Elisa BOURG <zaza3355@hotmail.fr>
2020-07-23 10:42:22 +0200

    ajout de compétences

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Skill;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +13,11 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $skillRepository = $this->getDoctrine()->getRepository(Skill::class);
+        $skills = $skillRepository->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'skills'         =>    $skills
         ]);
     }
 }
